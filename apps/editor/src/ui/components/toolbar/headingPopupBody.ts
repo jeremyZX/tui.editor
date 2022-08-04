d51ab4fc0baacfbc04b24ccf7374507be07830ea
Jeremy Frey <jeremy.m.frey@gmail.com>
2022-08-04 09:25:48 -0400

    feat(a11y): improve accessibility of headings popup
    Headings popup now supports keyboard interactivity (via tab key)
    and has menu/menuitem roles.
    
    Note that changes are required beyond the scope of this commit for full
    accessibility, such as an aria-haspopup/aria-controls on the menu
    opener, but these require ID values, which appear to not be supported at
    this time.

--- apps/editor/src/ui/components/toolbar/headingPopupBody.ts
+++ apps/editor/src/ui/components/toolbar/headingPopupBody.ts
@@ -1,9 +1,9 @@
 import { Emitter } from '@t/event';
 import { ExecCommand } from '@t/ui';
-import { closest } from '@/utils/dom';
 import i18n from '@/i18n/i18n';
 import html from '@/ui/vdom/template';
 import { Component } from '@/ui/vdom/component';
+import { cls } from '@/utils/dom';
 
 interface Props {
   eventEmitter: Emitter;
@@ -11,31 +11,32 @@ interface Props {
 }
 
 export class HeadingPopupBody extends Component<Props> {
-  execCommand(ev: MouseEvent) {
-    const el = closest(ev.target as HTMLElement, 'li')! as HTMLElement;
-
+  execCommand(level: number | null) {
     this.props.execCommand('heading', {
-      level: Number(el.getAttribute('data-level')),
+      level,
     });
   }
 
   render() {
     return html`
-      <ul
-        onClick=${(ev: MouseEvent) => this.execCommand(ev)}
-        aria-role="menu"
-        aria-label="${i18n.get('Headings')}"
-      >
+      <ul role="menu" aria-label="${i18n.get('Headings')}" class="${cls('menu-headings')}">
         ${[1, 2, 3, 4, 5, 6].map(
-          (level) =>
-            html`
-              <li data-level="${level}" data-type="Heading" aria-role="menuitem">
-                <${`h${level}`}>${i18n.get('Heading')} ${level}</$>
-              </li>
-            `
+          (level) => html`
+            <li data-type="Heading" role="none">
+              <${`h${level}`} role="presentation">
+                <button type="button" role="menuitem" onClick=${() => this.execCommand(level)}>
+                  ${i18n.get('Heading')} ${level}
+                </button>
+              </$>
+            </li>
+          `
         )}
-        <li data-type="Paragraph" aria-role="menuitem">
-          <div>${i18n.get('Paragraph')}</div>
+        <li data-type="Paragraph" role="none">
+          <div>
+            <button type="button" role="menuitem" onClick=${() => this.execCommand(null)}>
+              ${i18n.get('Paragraph')}
+            </button>
+          </div>
         </li>
       </ul>
     `;
